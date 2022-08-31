a33fbf9815623c759e07cb4f362c9ff5b0153b27
unknown <you@example.com>
2022-09-01 00:56:11 +0600

    product lisitng

---
+++ js/api.js
@@ -0,0 +1,44 @@
+// By Default Product Display 
+const loadData = () => {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+    .then(res => res.json())
+    .then(data => displayData(data.drinks))
+}
+const displayData = displayAll =>{
+    const getDiv = document.getElementById('product-div');
+    displayAll.map(items =>{ 
+      const createDiv = document.createElement('div');
+      createDiv.innerHTML = `
+      <div class="flex justify-center">
+          <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img class="rounded-t-lg" src="${items.strDrinkThumb}" alt=""/>
+            </a>
+            <div class="p-6">
+              <h5 class="text-gray-900 text-xl font-medium mb-2">${items.strGlass}</h5>
+              <p class="text-gray-700 text-base mb-4">
+                ${items.strInstructions.slice(0, 100)}
+              </p>
+              <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+            </div>
+          </div>
+      </div>
+    `;
+    getDiv.appendChild(createDiv);
+    }) 
+}
+loadData();
+// Search Drinks By Name 
+const searchData = () => {
+  const getSearchField = document.getElementById('search-navbar');
+  const getValue = getSearchField.value;
+   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${getValue}`
+   fetch(url)
+   .then(res => res.json())
+   .then(data => data)
+}
+document.getElementById('search-navbar').addEventListener('keypress', function (e) {
+    if(e.key === "Enter") {
+      searchData()
+    }
+})
