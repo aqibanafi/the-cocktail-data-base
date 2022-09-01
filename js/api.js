54d5217112f39963ab84f164351c6b4fc0afc991
unknown <you@example.com>
2022-09-01 21:24:15 +0600

    Modal Added

--- js/api.js
+++ js/api.js
@@ -33,15 +33,59 @@ const displayData = (displayValue, dataLimit) => {
                 <p class="text-gray-700 text-base mb-4">
                   ${items.strInstructions.slice(0, 20)}
                 </p>
-                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                <button type="button" onclick="(${items.idDrink})"
+                class="inline-block px-4 py-2.5 bg-red-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
+                data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                Get Detail
+                </button>
               </div>
             </div>
         </div>
       `;
       getDiv.appendChild(createDiv);
     })
+    // Modal Body 
+const modalBody = document.getElementById('modal-body');
+const createModalBody = document.createElement('table');
+createModalBody.innerHTML = `
+  <table class="min-w-full">
+  <thead class="border-b">
+    <tr>
+      <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+        Item ID
+      </th>
+      <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+      Ingredient 01
+      </th>
+      <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+      Ingredient 02
+      </th>
+      <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+      Ingredient 03
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="border-b">
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${items.idDrink}</td>
+      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        ${items.strIngredient1}
+      </td>
+      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        ${items.strIngredient2}
+      </td>
+      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        ${items.strIngredient3}
+      </td>
+    </tr>
+  </tbody>
+  </table>
+`;
+modalBody.appendChild(createModalBody);
   }
+  
 }
+
 function searchData (dataLimit) {
   const inputField = document.getElementById('search-navbar');
   const getInputValue = inputField.value;
